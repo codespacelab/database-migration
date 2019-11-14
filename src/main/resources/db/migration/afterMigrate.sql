d2178ae008814b0460254d4fe7e0af09068ca1ba
codespacelab <courses@codespacelab.com>
2019-11-14 22:35:34 +0000

    after migrate callback example

---
+++ src/main/resources/db/migration/afterMigrate.sql
@@ -0,0 +1,3 @@
+INSERT IGNORE INTO coffee (id, name, cup_size, price) VALUES (1, "Latte", "Medium", 2.99);
+INSERT IGNORE INTO coffee (id, name, cup_size, price) VALUES (2, "Americano", "Large", 3.99);
+INSERT IGNORE INTO coffee (id, name, cup_size, price) VALUES (3, "Capuchino", "Small", 1.99);
